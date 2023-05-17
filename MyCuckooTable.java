08015715ddb63e3dfa22cd7ea0711393425c9d2b
Keegan Sanders <46981601+sanderskn11@users.noreply.github.com>
2023-05-16 19:46:25 -0700

    Main to working

--- MyCuckooTable.java
+++ MyCuckooTable.java
@@ -1,44 +1,39 @@
 import java.util.Hashtable;
 
 public class MyCuckooTable<K, V> {
-
-    Hashtable myTable;
-
-    public static final int TABSIZE = 1 << 16;
-
-    private static final int MaxLoop = (int) Math.ceil(3 * Math.log(TABSIZE) / Math.log(2));
-
+    Hashtable<K,V> myTable;
+    public static final int TAB_SIZE = 1 << 16;
+    /**
+     * max length of the cuckoo key loop, (3lg2^16)=48.
+     */
+    private static final int MaxLoop = (int) Math.ceil(3 * Math.log(TAB_SIZE) / Math.log(2));
     private final SipHash sh = new SipHash();
-
-    private int mySize;
-
-    MyCuckooTable() {
-
-        myTable = new Hashtable<K, V>(TABSIZE);
-        mySize = 0;
-
+    public MyCuckooTable() {
+        myTable = new Hashtable<K, V>(TAB_SIZE);
+        // control code
     }
-
     public int size() {
-        return mySize;
+        return myTable.size();
     }
-
     public void reset() {
-
+        new MyCuckooTable<K,V>();
     }
-
     public boolean put(K searchKey, V newValue) {
+//        myTable.put(searchKey, newValue);
+        // all 0-255 one char strings
+        // control code at 256 empty string
+        // code length set to 9 bits
         return false;
     }
-
     public V get() {
         return null;
     }
-
     private int hash(K key, int fno) {
         // use this instead of hashCode() method
+        // take key
+        // if (fno = 1) return h1(K)
+        // else if (fno = 2) return h2(K)
+        // return int [0 - (cap-1)]
         return 0;
     }
-
-
 }
